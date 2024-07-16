9ad63024e2cdffb8c36646ae4749c2678f52420f
mcoooo <mcoooo10@hotmail.com>
2024-07-16 16:36:25 +0100

    comment out packages

--- flake.nix
+++ flake.nix
@@ -112,13 +112,13 @@
       };
 
       # Custom packages; acessible via 'nix build', 'nix shell', etc
-      packages = libx.forAllSystems (
-        system:
-        let
-          pkgs = unstable.legacyPackages.${system};
-        in
-        # import ./pkgs { inherit pkgs; }
-      );
+      # packages = libx.forAllSystems (
+      #   system:
+      #   let
+      #     pkgs = unstable.legacyPackages.${system};
+      #   in
+      #   # import ./pkgs { inherit pkgs; }
+      # );
 
       # Custom overlays
       # overlays = import ./overlays { inherit inputs; };
